5dc26444530bf08f24130ba649adf66fc4cdf307
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-29 20:20:33 +0800

    Poet 列表

---
+++ AncientPoetry.Web/Controllers/PoetController.cs
@@ -0,0 +1,29 @@
+﻿using AncientPoetry.IRepository.BaseFiles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AncientPoetry.Web.Controllers
+{
+    /// <summary>
+    /// 诗人控制器
+    /// </summary>
+    public class PoetController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PoetController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            var poets = _unitOfWork.Poet.GetAll();
+            return View(poets);
+        }
+
+
+
+    }
+}
