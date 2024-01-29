ff6c49ad65f346e33cedd93ab5e046402121fc68
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-29 19:58:59 +0800

    PoetVM

---
+++ AncientPoetry.Entities/ViewModels/PoetVM.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AncientPoetry.Entities.ViewModels
+{
+    public class PoetVM
+    {
+        public Poet? Poet { get; set; }
+
+        public IEnumerable<SelectListItem> DynastyList { get; set; }
+    }
+}
