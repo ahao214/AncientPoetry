cc034d92eb22adaca4cbab9eeeb3c13a0da36dad
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-29 19:50:33 +0800

    诗人表

---
+++ AncientPoetry.Entities/Poet.cs
@@ -0,0 +1,28 @@
+﻿using AncientPoetry.Entities.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AncientPoetry.Entities
+{
+    /// <summary>
+    /// 诗人表
+    /// </summary>
+    public class Poet : BaseEntity
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string? Description { get; set; }
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+        [ForeignKey("Dynasty")]
+        public int DynastyId { get; set; }
+        public Dynasty Dynasty { get; set; }
+
+    }
+}
