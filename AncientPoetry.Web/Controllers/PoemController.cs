fc5defd3ccf441cb0e291708a09dfb93d387693f
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-31 18:19:01 +0800

    PoemController

---
+++ AncientPoetry.Web/Controllers/PoemController.cs
@@ -0,0 +1,23 @@
+﻿using AncientPoetry.IRepository.BaseFiles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AncientPoetry.Web.Controllers
+{
+    /// <summary>
+    /// 诗歌控制器
+    /// </summary>
+    public class PoemController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PoemController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
