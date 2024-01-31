0eb7ba0047b69675629ec5079fc4f26f2109b4fd
liupenghao@fulan.com.cn <haochen214@163.com>
2024-01-31 19:35:04 +0800

    问题控制器

---
+++ AncientPoetry.Web/Controllers/ProblemController.cs
@@ -0,0 +1,57 @@
+﻿using AncientPoetry.IRepository.BaseFiles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AncientPoetry.Web.Controllers
+{
+    /// <summary>
+    /// 问题控制器
+    /// </summary>
+    public class ProblemController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProblemController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+
+        public IActionResult Index()
+        {
+            var problem = _unitOfWork.Problem.GetAll();
+            return View(problem);
+        }
+
+
+        #region 创建
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+        #region 编辑
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+        #region 删除
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        #endregion
+
+
+    }
+}
